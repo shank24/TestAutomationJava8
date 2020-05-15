228cdf45099fbb0d6c1f1b698b8d09f34fc722d8
Shank24 <shankkalra@gmail.com>
2020-05-15 21:45:19 +0530

    Assignment_1

--- src/test/java/com/udemy/java/test/assignments/TestMain_Ass_1.java
+++ src/test/java/com/udemy/java/test/assignments/TestMain_Ass_1.java
@@ -4,15 +4,41 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class TestMain_Ass_1 {
 
     public static void main(String[] args) throws IOException {
 
-        ///home/shanky/Personal/JAVA
-
         Path path = Paths.get("/home/shanky/Personal/JAVA/firstname.txt");
-        Files.readAllLines(path);
+        List<String> list = Files.readAllLines(path);
+
+        // Print the count of Names, starting with B
+
+        long countB = list.
+                stream()
+                .filter(s -> s.startsWith("B"))
+                .count();
+
+        System.out.println(countB);
+
+        // Create a list of Name, which starts with C and contains 'S'
+
+        long countChasS = list.
+                stream().filter(s -> s.startsWith("C")).
+                filter(s -> s.toLowerCase().contains("s")).
+                count();
+
+        System.out.println(countChasS);
+
+
+        // Print the total number of char all the names start with 'M'
+
+        list.stream().filter(s-> s.startsWith("M")).
+
+
+
+
 
     }
 }
